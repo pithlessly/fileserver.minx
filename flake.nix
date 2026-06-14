ce0dde47d9dc86ae5f9d90ab2195b16ce9b76f1a
pithlessly <79002325+pithlessly@users.noreply.github.com>
2026-06-14 12:42:29 -0700

    nix: build with naersk

--- flake.nix
+++ flake.nix
@@ -4,23 +4,32 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... } @ inputs:
-  flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShells.default = pkgs.mkShell {
-        nativeBuildInputs = [
-          pkgs.rustc
-          pkgs.cargo
-          pkgs.clippy
-          pkgs.rust-analyzer
-          pkgs.rustfmt
-        ];
-      };
-    }
-  );
+  outputs = { self, nixpkgs, flake-utils, naersk, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        naersk' = pkgs.callPackage naersk {};
+      in
+      {
+        packages.default = naersk'.buildPackage {
+          src = ./.;
+        };
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.rustc
+            pkgs.cargo
+            pkgs.clippy
+            pkgs.rust-analyzer
+            pkgs.rustfmt
+          ];
+        };
+      }
+    );
 }
